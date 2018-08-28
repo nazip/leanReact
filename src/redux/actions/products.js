0336dafff82d78877d3bdc189895504d02ca6a3c
nazip <nazipm@mail.ru>
2018-08-28 17:10:01 +0300

    add actions

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -1,19 +1,43 @@
 import * as types from '../const/actionTypes/products';
+import {dispatch} from 'redux';
+import request from 'superagent';
+import host from '/src/constants/Host';
 
-export const requstProducts = () => {
+const requstProducts = () => {
     return {
         type: types.FETCH_PRODUCTS_REQUEST
     }
 }
 
-export const errorProducts = () => {
+const errorProducts = (error) => {
     return {
-        type: types.FETCH_PRODUCTS_FAILURE
+        type: types.FETCH_PRODUCTS_FAILURE,
+        error
     }
 }
 
-export const successProducts = () => {
+const successProducts = (items) => {
     return {
-        type: types.FETCH_PRODUCTS_SUCCESS
+        type: types.FETCH_PRODUCTS_SUCCESS,
+        items
     }
+}
+
+export const fetchProducts = () => {
+    dispatch(requstProducts());
+    request
+    .get(`${host}/products`)
+    .timeout({
+        response: 10000,  
+        deadline: 60000, 
+    })
+    .then(
+        (res) => this.setState({items:res.body}),
+    (err) => {
+        if(err.timeout) {
+            dispatch(errorProducts('timeout'))
+        } else {     
+            dispatch(errorProducts(error))
+        }
+    }) 
 }
