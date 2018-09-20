7140d69443f8cd28d943d76b8ed2ec86b6d3e5bf
Nazip <nazipm@mail.ru>
2018-09-20 22:12:32 +0300

    in work

--- config/webpack-prod-config.js
+++ config/webpack-prod-config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ManifestPlugin = require('webpack-manifest-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const paths = require("./paths");
@@ -16,18 +16,24 @@ module.exports = merge(common, {
   },
   output: {
     filename: "[chunkhash]_[name].js",
-    path: paths.appBuild,
+    path: paths.appBuild
     // publicPath: "/assets/"
   },
   plugins: [
     new ManifestPlugin(),
-    new UglifyJSPlugin(),
+    // new UglifyJSPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production")
       }
     }),
-    new ExtractTextPlugin("styles.css"),
+    
     new CleanWebpackPlugin([paths.appBuild],{
       root: paths.appRoot,
       verbose: true,
@@ -42,7 +48,6 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        // include: path.resolve(paths.appSrc),
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -52,24 +57,11 @@ module.exports = merge(common, {
         }
       },
       {
-        test: /\.(css|scss)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                discardDuplicates: true,
-                sourceMap: false,
-                // This enables local scoped CSS based in CSS Modules spec
-                modules: true,
-                // generates a unique name for each class (e.g. app__app___2x3cr)
-                localIdentName: "[name]__[local]___[hash:base64:5]"
-              }
-            }
-            // Add additional loaders here. (e.g. sass-loader)
-          ]
-        })
+        test: /\.(css)$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" }
+        ]
       }
     ]
   }
