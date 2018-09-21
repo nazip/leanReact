c4cc3783d6c710bb2b5557f357b793f2b6781378
nazip <nazipm@mail.ru>
2018-09-21 17:04:45 +0300

    add pm2 conf

---
+++ pm2.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [{
+        name: 'leanReact',
+        script: './initializers/server/index.js',
+        cwd: '/home/nazip/deploy/leanReact/current',
+        instance: 2,
+        exec_mode: 'cluster'
+    }],
+    deploy: {
+        production: {
+            user: 'nazip',
+            host: ['localhost'],
+            ref: 'origin/part9',
+            repo: 'git@github.com:nazip/leanReact.git',
+            'post-deploy': 'npm install && npm run dev:build && pm2 startOrRestart pm2.config.js --env production',
+            path: '/home/nazip/deploy/leanReact',
+            ssh_options: 'StrictHostKeyChecking=no'
+        }      
+    }   
+};
