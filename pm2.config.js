a664a79ca145d9d19a2c672aec958d813a51a9f7
Nazip <nazipm@mail.ru>
2018-09-21 22:11:15 +0300

    change ip api

--- pm2.config.js
+++ pm2.config.js
@@ -12,7 +12,7 @@ module.exports = {
             host: ['localhost'],
             ref: 'origin/part9',
             repo: 'git@github.com:nazip/leanReact.git',
-            'post-deploy': 'npm install && npm run dev:build && pm2 startOrRestart pm2.config.js --env production',
+            'post-deploy': 'npm install && npm run prod:build && pm2 startOrRestart pm2.config.js --env production',
             path: '/home/nazip/deploy/leanReact',
             ssh_options: 'StrictHostKeyChecking=no'
         }      
